caa10c6e874597cce6446d38980b4938aa43f08f
liz <brickyjenkins@gmail.com>
2018-01-12 20:58:38 -0600

    static/html - removed header and footer components so that each html file only contains the content code index.php - has 3 sections: header, footer, and content section. The content section loads the appropriate html file based on the user provided url
    static/html/home.html - created this file to load the appropriate index page's content

--- index.php
+++ index.php
@@ -9,21 +9,50 @@
 #$db = new mysqli('localhost','root','','pet_shop');
 #print_r($db);
 
+# header
 include_once('static/html/header.html');
-?>
-<div id="content" style="margin-bottom: 200px">
 
-    <div id="greeting">
+####################################################
+# html content parent folder
+####################################################
+$content_prefix = 'static/html/';
 
-        <p>The Best a Pet Can Get</p>
+####################################################
+# determine what page the user requested
+####################################################
+$page = NULL;
+if (isset($_GET['page'])) {
+    # set the value of the page
+    $page = $_GET['page'];
+}
 
-    </div>
+####################################################
+# switch statement to control what html page get loaded
+####################################################
 
-    <div id="button">
-        <a href="grooming.html"><button type="button">Start Grooming!</button></a>
-    </div>
+switch ($page) {
+    case "about":
+        $content_page = 'about.html';
+        break;
+    case "grooming":
+        $content_page = 'grooming.html';
+        break;
+    case 'location':
+        $content_page = 'location.html';
+        break;
+    case 'contact':
+        $content_page = 'contact.html';
+        break;
+    default:
+        # default page
+        $content_page = 'home.html';
+}
+####################################################
+# $content_path = 'static/html/home.html'
+####################################################
+$content_path = $content_prefix . $content_page;
+include_once($content_path);
 
-</div>
-<?php
+# footer
 include_once('static/html/footer.html');
 ?>
